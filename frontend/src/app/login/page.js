95ca9d0a32e25a6713b6f6f11887c93a05eb39ba
ys-solutions <yssolutiondigit@gmail.com>
2026-07-05 18:18:46 +0300

    première envoie après test réussi de la frontend

---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center">
+      <h1 className="text-5xl text-white font-bold">
+        LOGIN PAGE
+      </h1>
+    </div>
+  );
+}
